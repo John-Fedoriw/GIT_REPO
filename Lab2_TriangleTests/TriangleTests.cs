4900036f9c640a07c040262cf779ee47e6bee8b2
John-Fedoriw <55063570+John-Fedoriw@users.noreply.github.com>
2019-10-17 18:35:22 -0400

    Passing boundary test for Find Area

--- Lab2_TriangleTests/TriangleTests.cs
+++ Lab2_TriangleTests/TriangleTests.cs
@@ -26,8 +26,8 @@ namespace Lab2_Triangle.Tests
         [TestMethod()]
         public void FindAreaTest()
         {
-            double B = 0.0;
-            Assert.IsTrue(B == 0.0);
+            double A = 0.0;
+            Assert.IsTrue(A == 0.0);
         }
     }
 }
