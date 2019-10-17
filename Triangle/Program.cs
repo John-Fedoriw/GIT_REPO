ad0dd1d284ad9b817cb126c7a7d854871b89af78
John-Fedoriw <jfedoriw1574@conestogac.on.ca>
2019-10-16 23:05:07 -0400

    Added Unit Test

---
+++ Triangle/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Triangle
+{
+    
+        public class Triangle
+    {
+
+
+
+        // variables for triangle
+
+        public double SideA = 0;
+
+        public double SideB = 0;
+
+        public double SideC = 0;
+
+        public double AngleA = 0;
+
+        public double AngleB = 0;
+
+        public double AngleC = 0;
+
+        public double tBase = 0;
+
+
+
+        public double FindAngle(double AngleA, double AngleB)
+
+        {
+            return (180.00 - AngleA - AngleB);
+
+        }
+
+
+
+       
+
+        //public double FindArea(double tBase, double height)
+
+        //{
+
+        //    return (0.5 * (height * tBase));
+
+        //}
+
+
+        //public void FindTriangleType(double SideA, double SideB, double SideC)
+
+        //{
+
+        //    if (SideA == SideB && SideB == SideC)
+
+        //    {
+
+        //        Console.WriteLine("Equalateral Triangle!!1\n");
+
+        //    }
+
+        //    else if (SideA == SideB || SideB == SideC || SideA == SideC)
+
+        //    {
+
+        //        Console.WriteLine("Iscoles triangle!!\n");
+
+        //    }
+
+        //    else
+
+        //    {
+
+        //        Console.WriteLine("Scalene triangle!!!\n");
+
+        //    }
+
+
+
+        //}
+
+    }
+}
+   
+
+   
---
+++ TriangleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriangleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriangleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("790a55e6-2828-431f-a16a-3284ef1fd225")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
